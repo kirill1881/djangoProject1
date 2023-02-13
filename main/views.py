ea74354cb767e5ce26855e6235e619af3525cfa2
kirillsergeev <kirillovk317@gmail.com>
2023-02-13 20:34:08 +0300

    123

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h2>Main page</h2>')
+    return render(request, 'index.html', {'title':'главная'})
+
+def about(request):
+    return render(request, 'about.html')
